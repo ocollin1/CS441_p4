2f9887bb9e33b73bdb6ab2382f7a34a4022e7264
Owen Collins <ocollin1@slic-38.pods.bu.int>
2019-03-11 15:44:36 -0400

    added code from p3 t act as library code for p4

---
+++ TowerDefense/Shot.m
@@ -0,0 +1,17 @@
+//
+//  Shot.m
+//  TowerDefense
+//
+//  Created by Owen Collins on 3/11/19.
+//  Copyright © 2019 Owen Collins. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
